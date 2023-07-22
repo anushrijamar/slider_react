27af2fb576b827d4a264858d4bbf018b26f01b03
anushrijamar <117384732+anushrijamar@users.noreply.github.com>
2023-07-22 21:26:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+//one function means one componenet
+// <> </> fragment is used to take place of div if container is not formed
+// import Button from "./components/Button";
+// import Box from "./components/box";
+// import Input from "./components/Input";
+// import InputLength from "./components/InputLength";
+// // import "./button.css";
+// function App() {
+//   return(<><InputLength /></>);  
+// }
+// export default App;
+// import Calci from "./components/Calci";
+// function App()
+// {
+//     return (<><Calci /></>);
+// }
+// export default App;
+//box compponent work aas wrapper that it holds various components
+// import Mui from "./components/Mui";
+// function App()
+// {
+//     return(
+//         <>
+//         <Mui />
+//         </>
+//     )
+// }
+// export default App;
+// import Dashboard from "./components/Dashboard";
+// function App()
+// {
+//     return (
+//         <>
+//         <Dashboard /></>
+//     );
+// }
+// export default App;
+import { Slider } from "@mui/material";
+import Resume from "./components/Resume";
+import ImgSlider from "./ImgSlider";
+
+function App()
+{
+   return (<ImgSlider/>)
+}
+export default App;
---
+++ ImgSlider.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import myImage from './components/dore.jpg';
+import myImage2 from './components/doremon copy.png'
+import myImage1 from './components/doru.jpg'
+import { useState,useEffect } from 'react';
+
+export const ImgSlider = () => {
+    const [showImage, setShowImage] = useState(false);
+    const [showImage1, setShowImage1] = useState(false);
+    const [showImage2, setShowImage2] = useState(false);
+    var i=0;
+      useEffect(() => {
+        const interval = setInterval(() => {
+            const myArray = [false, false, false];
+            myArray[i] = true;
+            setShowImage(myArray[0]);
+            setShowImage1(myArray[1]);
+            setShowImage2(myArray[2]);
+            i++;
+            if(i==3)
+            i=0;
+            console.log('Button clicked!');
+        }, 1000); 
+        return () => {
+          clearInterval(interval);
+        };
+      }, []); 
+    const divStyle = {
+        backgroundColor: 'blue',
+        color: 'white',
+        // padding: '10px',
+        width:'100%',
+        height:'450px',
+      };
+  return (
+    <div >
+    {showImage &&<img src={myImage} style={divStyle}></img>}
+    {showImage1 && <img src={myImage1} style={divStyle}></img> }
+    {showImage2 && <img src={myImage2} style={divStyle}></img> }
+    {/* <button onClick={next} >Next</button> */}
+    </div>
+  )
+}
+export default ImgSlider;
